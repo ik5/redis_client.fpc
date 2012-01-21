76c360d1755e71a208814b5b3af64b15efb2be1e
ik5 <idok@linesip.com>
2012-01-21 21:50:08 +0200

    Working on the commands for Redis

---
+++ src/rd_commands.pas
@@ -0,0 +1,44 @@
+(*
+  Redis Commands implementation for Object Pascal
+
+  Copyright (C) 2012 Ido Kanner (idokan at@at gmail dot.dot com)
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or (at your
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you must extend this exception to your version of the library.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*)
+unit rd_commands;
+
+{$IFDEF FPC}
+{$mode objfpc}
+{$ENDIF}
+{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, rd_protocol;
+
+implementation
+
+end.
+
---
+++ tests/test_commands.lpr
@@ -0,0 +1,13 @@
+program test_commands;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, sysutils, laz_synapse, rd_protocol, rd_commands
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+end.
+
