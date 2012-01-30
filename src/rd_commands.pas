dd9dc926d4ddd3987dfb0ed175e58617176f8ac0
ik5 <idok@linesip.com>
2012-01-30 13:56:25 +0200

    Have support for AUTH. TODO: create a better function for this, more DRY

--- src/rd_commands.pas
+++ src/rd_commands.pas
@@ -182,7 +182,7 @@ function TRadisDB.Auth(const APass: String): TRedisReturnType;
 var return : string;
 begin
   return := send_command('AUTH', [APass]);
-
+  Result := FRedisParser.ParseLine(return);
 end;
 
 { TRedisObject }
