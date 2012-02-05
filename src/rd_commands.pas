45b6d845143aeb29b02ec19566c4574f973863a9
ik5 <idok@linesip.com>
2012-02-05 13:05:41 +0200

    Added documentation for info, but now I really have to rewrite the parser to make it work.

--- src/rd_commands.pas
+++ src/rd_commands.pas
@@ -619,6 +619,42 @@ type
     function FlushDB : TRedisReturnType; virtual;
 
     (*
+       The INFO command returns information and statistics about the server in
+       format that is simple to parse by computers and easy to red by humans.
+
+       All the fields are in the form of field:value terminated by CRLF.
+
+       Returns:
+         * TRedisBulkReturnType with a list of field:value terminated by CRLF
+         * nil on exception
+
+        Exceptions:
+        * ERedisException - When something went wrong in the parsing or with
+                            the socket
+
+       Notes:
+        * used_memory is the total number of bytes allocated by Redis using its
+          allocator (either standard libc malloc, or an alternative allocator
+          such as tcmalloc
+
+        * used_memory_rss is the number of bytes that Redis allocated as seen
+          by the operating system. Optimally, this number is close to
+          used_memory and there is little memory fragmentation. This is the
+          number reported by tools such as top and ps. A large difference
+          between these numbers means there is memory fragmentation.
+          Because Redis does not have control over how its allocations are
+          mapped to memory pages, used_memory_rss is often the result of a spike
+          in memory usage. The ratio between used_memory_rss and used_memory is
+          given as mem_fragmentation_ratio.
+
+        * changes_since_last_save refers to the number of operations that
+          produced some kind of change in the dataset since the last time either
+          SAVE or BGSAVE was called.
+
+        * allocation_stats holds a histogram containing the number of
+          allocations of a certain size (up to 256). This provides a means of
+          introspection for the type of allocations performed by Redis at run
+          time.
 
      *)
     function info : TRedisReturnType; virtual;
